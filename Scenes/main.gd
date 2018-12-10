54d1e87b92d2f146dd3c5b7e20db2c703abdbe66
Chubak Bidpaa <aurlito@riseup.net>
2018-12-10 18:18:00 +0330

    done

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -3,10 +3,12 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var blocks
+signal all_destroyed
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("music").play()
+	get_node(".").connect("all_destroyed", self, "_all_destroyed")
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +16,11 @@ func _process(delta):
 #	pass
 	if not get_node("ball").launch:
 		get_node("ball").position = get_node("paddle").position
+	blocks = get_tree().get_nodes_in_group("blocks").size()
+	get_node("Label").text = str(blocks)
 	
-	get_node("Label").text = str(get_node("block_1").destroyed)
+	if blocks == 0:
+		emit_signal("all_destroyed")
 	
+func _all_destroyed():
+	get_node("Control").visible = true
